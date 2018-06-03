cb74df11b59073b81e3843b1bb049dd1ba6ef935
Clément Habinshuti <hello@habbes.xyz>
2018-06-03 15:23:40 +0300

    Close worker after execution

--- worker.js
+++ worker.js
@@ -11,9 +11,11 @@ const workerPort = {
         }
     }
 };
+
 onmessage = function (e) {
     console.log('worker job triggered');
     const execute = new Function('app', e.data.code);
     // expose the worker port as the app variable in the user code
     execute(workerPort.app);
+    close();
 };
